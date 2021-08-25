679f48a398579ced612ed84bcb56cc93f29afb3c
Dejan Svetec <dejan.svetec@protonmail.com>
2021-08-25 11:32:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+    let body = core.getInput('body');
+    const override = core.getInput('override') == 'true';
+    const ghToken = core.getInput('ghToken');
+    const octokit = github.getOctokit(ghToken);
+    const prNumber = github.context.payload.number;
+    const repository = github.context.payload.repository;
+
+    if (!override) {
+        const { data: pullRequest } = await octokit.rest.pulls.get({
+            repo: repository.name,
+            owner: repository.owner.login,
+            pull_number: prNumber,
+        });
+
+        if (pullRequest.body) {
+            body += `\n\n${pullRequest.body}`;
+        }
+    }
+
+    try {
+        await octokit.rest.pulls.update({
+            repo: repository.name,
+            owner: repository.owner.login,
+            pull_number: prNumber,
+            body: body,
+        });
+    } catch (error) {
+        core.setFailed(error);
+    }
+}
+
+run();
